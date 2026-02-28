aff21b2c934a3800bc3c6826b5b2c0960b9d4962
GitHub Action <action@github.com>
2026-02-28 19:37:07 +0000

    Bump version: 0.6.2 → 0.7.0

--- ic_python_db/__init__.py
+++ ic_python_db/__init__.py
@@ -19,7 +19,7 @@ from .properties import (
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.6.2"
+__version__ = "0.7.0"
 __all__ = [
     "Database",
     "Entity",

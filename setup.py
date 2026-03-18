5186a13b2445ffbb348ec9ffda298064550b267e
GitHub Action <action@github.com>
2026-03-18 07:10:58 +0000

    Bump version: 0.7.4 → 0.7.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ic_python_db",
-    version="0.7.4",
+    version="0.7.5",
     author="Smart Social Contracts",
     author_email="smartsocialcontracts@gmail.com",
     description="A lightweight key-value database with entity relationships and audit logging",

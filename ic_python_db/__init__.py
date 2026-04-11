0b3398ce2a70d9c978615cb3bb645280243f567d
deucalioncodes <jmpmonteagudo@gmail.com>
2026-04-11 18:08:16 +0200

    Bump version: 0.7.7 → 0.7.8

--- ic_python_db/__init__.py
+++ ic_python_db/__init__.py
@@ -19,7 +19,7 @@ from .properties import (
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.7.7"
+__version__ = "0.7.8"
 __all__ = [
     "Database",
     "Entity",

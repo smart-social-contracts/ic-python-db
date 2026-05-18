7d8c3e71cc3668d0447b41626c4f334db909110f
deucalioncodes <deucalioncodes@users.noreply.github.com>
2026-05-18 16:29:36 +0000

    Bump version: 0.9.0 → 0.9.1

--- ic_python_db/__init__.py
+++ ic_python_db/__init__.py
@@ -20,7 +20,7 @@ from .schema import SchemaIncompatibleError, build_schema, diff_schemas, schema_
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = [
     "Database",
     "Entity",
